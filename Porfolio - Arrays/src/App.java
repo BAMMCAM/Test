dd4660a215cdc3c385e1546a22049b299f5159a2
BAMMCAM <30834136+BAMMCAM@users.noreply.github.com>
2017-09-14 16:36:33 +0100

    Add files via upload

---
+++ Porfolio - Arrays/src/App.java
@@ -0,0 +1,28 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int[] values = new int[3];
+
+		values[0] = 10;
+		values[1] = 20;
+		values[2] = 30;
+
+		System.out.println(values[0]);
+		System.out.println(values[1]);
+		System.out.println(values[2]);
+		
+		for(int i = 0; i < values.length; i++) {
+			System.out.println(values[i]);
+		}
+		
+		int[] numbers = {5, 6, 7, 8};
+		
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+
+	}
+
+}
